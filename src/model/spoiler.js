00b61271a608d33be50016dd12e3ccc0d20bc7ec
Romulo Sousa <romulosousa27@hotmail.com>
2019-05-02 18:44:13 -0300

    Propriedades da model Spoiler

---
+++ src/model/spoiler.js
@@ -0,0 +1,35 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../database/database');
+
+/** Setting Model properties */
+const Spolier = sequelize.define("spoiler", {
+    id:{
+        allowNull: false, 
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+    },
+    title:{
+        allowNull: false,
+        type: Sequelize.STRING(255),
+        validade:{
+            len: [2, 255]
+        }
+    },
+    user:{
+        allowNull: false,
+        type: Sequelize.STRING,
+        validade:{
+            len:[2, 40]
+        }
+    },
+    description:{
+        allowNull: false,
+        type: Sequelize.STRING,
+        validade:{
+            len:[2, 255]
+        }
+    }
+});
+
+module.exports = Spoiler;
