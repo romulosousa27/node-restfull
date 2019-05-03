4cd0c6c43ab409f7cab52d718817c55fa0455cc1
Romulo Sousa <romulosousa27@hotmail.com>
2019-05-03 12:22:01 -0300

    Modules para o CRUD da api

--- src/controller/ControllerSpoiler.js
+++ src/controller/ControllerSpoiler.js
@@ -3,6 +3,7 @@ const Spoiler =  require("../model/spoiler");
 /** 
  *  Middleware 
  *  Retorna um dado Spoiler 
+ * 
  */
 exports.findSpoiler = (req, res, next) => {
     const id = req.params.id
@@ -18,9 +19,11 @@ exports.findSpoiler = (req, res, next) => {
     }).catch(error => next(error));
 };
 
+
 /**
  *  Middleware 
  *  Retorna todos os dados Spolier com paginação e limite
+ * 
 */
 exports.allSpoiler = (req, res, next) => {
     let limit = parseInt(req.query.limit || 0);
@@ -38,4 +41,87 @@ exports.allSpoiler = (req, res, next) => {
     Spoiler.findAll( {limit: limit, offset: page} ).then(spoilers => {
         res.send(spoilers);
     }).catch(error => next(error));
-}
+};
+
+
+/**
+ * Middleware
+ * Criando um novo Spoiler
+ * 
+ */
+exports.createSpoiler = (req, res, next) => {
+    const title = req.body.title;
+    const user = req.body.user;
+    const description = req.body.description;
+
+    Spoiler.create({
+        title: title,
+        user: user,
+        description: description,
+
+    }).then( ()=>{ 
+        res.status(201).send();
+    }).catch((error) => next(error))
+};
+
+
+/** 
+ * Middleware
+ * Atualizando um Spoiler
+ * 
+ * @param id
+ * 
+ */
+exports.updateSpoiler = (req, res, next) => {
+    const id = req.params.id;
+
+    const title = req.body.title;
+    const user = req.body.user;
+    const description = req.body.description;
+
+    Spoiler.findById(id).then( spoiler => {
+        if (spoiler) {
+            Spoiler.update(
+            {
+                title: title,
+                user: user,
+                description: description,
+            },
+            {
+                where: {id : id}
+            }).then( () => {
+                res.send();
+            }).catch(error => next(error));
+
+        } 
+        else {
+            res.status(404).send();
+        }
+    }).catch(error => next(error));
+};
+
+
+/**
+ * Middleware 
+ * Deletando um Spoiler
+ * 
+ * @param id
+ * 
+ */
+ exports.deleteSpoiler = (req, res, next) => {
+     const id =  req.params.id;
+
+     Spoiler.findById(id).then( spoiler => {
+         if (spoiler) {
+            Spoiler.destroy(
+            {
+                where: { id : id}
+            }).then( () => {
+                res.send();
+            }).catch(error => next(error));
+         } 
+         else {
+            res.status(404).send();
+         }
+     }).catch(error => next(error));
+ }
